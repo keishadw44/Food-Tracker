d148c8fbe3f0ff230da270f48030c2ad2df67795
byron fisher <fbyron710@gmnail.com>
2022-10-18 21:04:14 -0400

    foodtrackerapp

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+
+
+// middleware that is specific to this router
 router.use(function timeLog (req, res, next){
-    console.log('Time: ', Date.now(), 'authentication.js');
+    console.log('Time: ', Date.now(), 'login.js');
     next();
 });
 
