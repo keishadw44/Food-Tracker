fde80ed9169656e179e99c864ac869807fbd7f72
Quyana Barrow <qbarrow@gmail.com>
2022-10-22 11:08:05 -0400

    it is working

--- routes/food.js
+++ routes/food.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("../models");
+const { User, Foodform } = require('../models')
 const router = express.Router();
 
 // const User = require('./helpers/dbConnection')
@@ -11,71 +11,88 @@ router.use(function timeLog(req, res, next) {
 
 
 function requireAuth(req, res, next) {
-  if (req.session.user) next();
-  else if (!req.session.user) {
-    req.session.destroy();
-    console.log("You are NOT logged in");
-    return req.redirect("/login");
-  } else {
-    req.session.destroy();
-    console.log("You are NOT logged in");
-    return res.redirect("./register");
-  }
+  if (req.session.user) next(); //remember to remove ! when fixed
+//   else if (!req.session.user) {
+//     req.session.destroy();
+//     console.log("You are NOT logged in");
+//     return req.redirect("/login");
+//   } else {
+//     req.session.destroy();
+//     console.log("You are NOT logged in");
+//     return res.redirect("./register");
+//   }
 }
 
-router.get("*", (req, res) => {
-  req.session.destroy();
-  if (!req.session) console.log("Session Destroyed");
-  return res.redirect(`home`);
-});
+// router.get("*", (req, res) => {
+//   req.session.destroy();
+//   if (!req.session) console.log("Session Destroyed");
+//   return res.redirect(`home`);
+// });
 
-router.get("/food", requireAuth, async (req, res) => {
-  const records = await User.findAll();
-  let users = [];
-  records.map((record) => {
-    users.push(record.dataValues);
-  });
+router.get("/food",  async (req, res) => {
+//   const records = await User.findAll();
+//   let users = [];
+//   records.map((record) => {
+//     users.push(record.dataValues);
+//   });
 
-  const formData = {
-    //attributes
-    username: "nameqb",
-    date: new Date(),
-    mealCategory: "lunch",
-    foodName: "apple",
-    beverage: "water",
-  };
+  const records = await Foodform.findAll({
+   where: {
+     userName: 'Keisha'
+   }
+ });
+  console.log(records);
 
-  console.log(formData.mealCategory);
+  
+
+//   const formData = [{
+//     //attributes
+//     userName: "nameqb",
+//     day: new Date(),
+//     mealCategory: "lunch",
+//     foodName: "apple",
+//     beverage: "water",
+//   }];
+
+//   console.log(formData.mealCategory);
 
   return res.render(`food`, {
-    userData: users,
-    formData: formData,
+   //  userData: users,
+    foodObject: records,
     title: "Food Daily",
   });
 });
 
-router.get('/foodform', requireAuth, function (req, res) {
+router.get('/foodform',  function (req, res) {
    return res.render(`foodform`, {
        title: "Food Form"
    })
 });
 
-router.post("/foodform", requireAuth, async (req, res) => {
+router.post("/foodform",  async (req, res) => {
 
   // try {`
     const { day, foodName, beverage, mealType } = req.body;
 
+    console.log(day)
+    console.log(new Date(day))
+    console.log(foodName)
+    console.log(beverage)
+    console.log(mealType)
+
     if (day && foodName && beverage && mealType) {
          console.log(day, foodName, beverage, mealType);
          try {
-           await db.Comment.create({
-            day: day,
+            console.log("Creating Food!!!")
+           await Foodform.create({
+            userName: 'Keisha',
+            day: new Date(day),
             foodName: foodName,
-            beverag: beverage,
+            beverage: beverage,
             mealType: mealType
            })
-           const records = await db.Comment.findAll()
-           res.send(records)
+           const records = await Foodform.findAll()
+         //   res.send(records)
           
          } catch (error) {
           console.log(error);
